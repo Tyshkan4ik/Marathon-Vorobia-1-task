c6ad8e460b04aa087add963fb5903312c2825147
Tyshkan4ik <001vat@gmail.com>
2023-03-01 13:10:41 +0300

    adding a view with constraints

--- Marathon from code Vorobia - 1 task/ViewController.swift
+++ Marathon from code Vorobia - 1 task/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var viewSquare: UIView = {
+        let view = UIView()
+        view.backgroundColor = .brown
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .white
+        setupView()
+        setupConstraints()
     }
-
-
+    
+    private func setupView() {
+        view.addSubview(viewSquare)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            viewSquare.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 100),
+            viewSquare.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            viewSquare.heightAnchor.constraint(equalToConstant: 150),
+            viewSquare.widthAnchor.constraint(equalToConstant: 150)
+        ])
+    }
+    
+    
+    
 }
 
