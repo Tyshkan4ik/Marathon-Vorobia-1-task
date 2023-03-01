fcc1813a8526f51f4e9bad00ad8347c2e3e76583
Tyshkan4ik <001vat@gmail.com>
2023-03-01 11:30:31 +0300

    Initial Commit

---
+++ Marathon from code Vorobia - 1 task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathon from code Vorobia - 1 task
+//
+//  Created by Виталий Троицкий on 01.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
