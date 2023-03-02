ad1c04611c03d6e466caaec8bb7d690490eb6b0e
Tyshkan4ik <001vat@gmail.com>
2023-03-02 12:35:16 +0300

    implementing a square in a class SquareGradientView

---
+++ Marathon from code Vorobia - 1 task/SquareGradientView.swift
@@ -0,0 +1,40 @@
+//
+//  SquareGradientView.swift
+//  Marathon from code Vorobia - 1 task
+//
+//  Created by Виталий Троицкий on 02.03.2023.
+//
+
+import UIKit
+
+class SquareGradientView: UIView {
+    
+    let gradient = CAGradientLayer()
+    
+    init (radius: CGFloat, colorGradient: [CGColor], shadowOpacity: Float) {
+        super.init(frame: .zero)
+        layer.shadowOpacity = 0.85
+        layer.shadowRadius = 20
+        
+        gradient.cornerRadius = 20
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.frame = bounds
+        var colors = [CGColor]()
+        for color in colorGradient {
+            colors.append(color)
+        }
+        gradient.colors = colors
+        layer.addSublayer(gradient)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradient.frame = bounds
+    }
+    
+}
